0ed301092692281f49dc197a0bd91ad7541d503e
Urmendizx <urmendiz.eu@gmail.com>
2022-02-14 23:12:03 -0500

    first exercise added

---
+++ One.java
@@ -0,0 +1,15 @@
+class One {
+    public static void main(String ...args) {
+        final int variablesAmount = 2;
+        int firstNumber;
+        int secondNumber;
+        int result;
+
+        firstNumber = 10;
+        secondNumber = 9;
+
+        result = (firstNumber + secondNumber) * variablesAmount;
+
+        System.out.println(result);
+    }
+}
