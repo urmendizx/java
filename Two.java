d8de921d04902ebe92763358421ffcdd016665e2
Urmendizx <urmendiz.eu@gmail.com>
2022-02-16 18:42:47 -0500

    second exercise added

---
+++ Two.java
@@ -0,0 +1,15 @@
+class Two {
+    public static void main(String ...args) {
+        // byte byteA = 5;
+        // byte byteB = 3;
+        // byte byteR = (byte) (byteA + byteB);
+
+        // System.out.println(byteR);
+
+        float floatA = 1.5f;
+        float floatB = 3.5f;
+        float floatR = floatA + floatB;
+
+        System.out.println(floatR);
+    }
+}
