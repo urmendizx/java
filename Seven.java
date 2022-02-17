9e1566d50fd893ddc46c828faaaa8a516bff465d
Urmendizx <urmendiz.eu@gmail.com>
2022-02-17 09:00:17 -0500

    seventh exercise added

---
+++ Seven.java
@@ -0,0 +1,14 @@
+class Seven {
+    public static void main(String ...args) {
+        for (byte i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        //Si se quiere inicializar fuera, y incrementar/disminuir dentro:
+
+        // byte i = 0;
+        // for (; i < 10;) {
+        //     System.out.println(++i);
+        // }
+    }
+}
