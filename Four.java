af90caff9065e62a039cbdd32bf6dedf870217d4
Urmendizx <urmendiz.eu@gmail.com>
2022-02-16 21:16:55 -0500

    fourth exercise added

---
+++ Four.java
@@ -0,0 +1,32 @@
+class Four {
+    public static void main(String ...args) {
+        byte day = 1;
+
+        switch (day) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miercoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("El dia no es valido");
+                break;
+        }
+    }
+}
